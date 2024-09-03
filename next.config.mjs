3c10575076c351d5898e98b85abe8ddf6745b97f
Songeun <hannahkeeien@gmail.com>
2024-09-03 15:25:32 +0900

    feat: update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const prefix =
+  process.env.NODE_ENV === "production" ? "https://q45402sk.github.io/" : "";
+
+const nextConfig = {
+  output: "export",
+  assetPrefix: prefix,
+};
 
 export default nextConfig;
